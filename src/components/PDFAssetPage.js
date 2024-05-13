d8ee6d685284737aa7589dfb6c1d399ffd892a0c
Utkarsh Sanjivan <74701697+utkarshsanjivan-conde@users.noreply.github.com>
2024-05-14 01:12:41 +0530

    feat(summarize): Added Support for PDF Summarize (#4)
    * Added Support for PDF Summarize
    
    * Added Support for PDF Summarize
    
    * Added Support for PDF Summarize
    
    * Added Support for PDF Summarize
    
    * Added Support for PDF Summarize
    
    * Added Support for PDF Summarize

--- src/components/PDFAssetPage.js
+++ src/components/PDFAssetPage.js
@@ -1,10 +1,40 @@
-import { useParams } from "react-router-dom";
-
+import React, { useState } from 'react';
+import axios from 'axios';
 const PDFAssetPage = () => {
-    const { id }  = useParams();
-    return <div style={{fontSize: '24px' }}>
-        This is PDF {id} asset edit page
-    </div>
-}
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [summarizedText, setSummarizedText] = useState('');
+  
+  const handleFileChange = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
+  
+  const handleSummarize = async () => {
+    try {
+      const formData = new FormData();
+      formData.append('file', selectedFile);
+      const response = await axios.post('http://127.0.0.1:5000/summarize', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      });
+      setSummarizedText(response.data);
+    } catch (error) {
+      console.error('Error summarizing text:', error);
+    }
+  };
 
+  return (
+    <div>
+      <input
+        type="file"
+        onChange={handleFileChange}
+      />
+      <button onClick={handleSummarize}>Summarize</button>
+      <div>
+        <h2>Summarized Text:</h2>
+        <p>{summarizedText}</p>
+      </div>
+    </div>
+  );
+};
 export default PDFAssetPage;
